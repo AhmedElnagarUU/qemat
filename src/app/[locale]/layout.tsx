ec7048b7763b5bccd9301642835046571a7c4622
ahmed <ahmedelnagaruu@gmail.com>
2025-03-22 05:40:31 +0200

    try fix local error typescript

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -21,23 +21,22 @@ export default async function LocaleLayout({
   params: { locale }
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params:{locale:unknown}
 }) {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale)) notFound();
+  if (!locales.includes(locale as string)) notFound();
 
   let messages;
   try {
     messages = (await import(`@/messages/${locale}.json`)).default;
   } catch {
-    
-    notFound();
+    return notFound();
   }
 
   return (
-    <html lang={locale} className="scroll-smooth">
+    <html lang={locale as string} className="scroll-smooth">
       <body className={`${locale === 'ar' ? 'font-arabic' : ''}`}>
-        <NextIntlClientProvider locale={locale} messages={messages} timeZone="Asia/Riyadh">
+        <NextIntlClientProvider locale={locale as string} messages={messages} timeZone="Asia/Riyadh">
           <div className="min-h-screen flex flex-col">
             <Navbar />
             {children}
